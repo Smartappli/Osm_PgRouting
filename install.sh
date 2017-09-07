76dcef5e5c3e1f1e36f57df69ca1b57e5bf4fcac
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2017-09-07 11:07:14 +0200

    redundancies elimination

--- install.sh
+++ install.sh
@@ -7,8 +7,7 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y osmosis postgresql-9.6 postgresql-9.6-postgis-2.3 postgresql-contrib-9.6
 sudo apt install -y git autoconf libtool libxml2-dev libbz2-dev libgeos-dev libgeos++-dev libproj-dev gdal-bin libgdal-dev g++ libmapnik-dev mapnik-utils python-mapnik
-sudo apt install -y fonts-noto-cjk fonts-noto-hinted fonts-noto-unhinted ttf-unifont fonts-dejavu-core fonts-droid-fallback ttf-unifont fonts-sipa-arundina fonts-sil-padauk fonts-khmeros fonts-beng-extra fonts-gargi fonts-taml-tscu fonts-tibetan-machine
-sudo apt install -y sudo apt-get install fonts-noto-cjk fonts-noto-hinted fonts-noto-unhinted fonts-hanazono ttf-unifont
+sudo apt install -y fonts-noto-cjk fonts-noto-hinted fonts-noto-unhinted fonts-hanazono ttf-unifont fonts-dejavu-core fonts-droid-fallback ttf-unifont fonts-sipa-arundina fonts-sil-padauk fonts-khmeros fonts-beng-extra fonts-gargi fonts-taml-tscu fonts-tibetan-machine
 # Tuning de postgresql
 sed 's/md5/trust/' /etc/postgresql/9.6/main/pg_hba.conf
 sed 's/peer/trust/' /etc/postgresql/9.6/main/pg_hba.conf
